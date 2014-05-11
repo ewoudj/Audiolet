931ef79be3636df403e62fd62349c3dd8e2aeeec
ewoudj <ewoudj@gmail.com>
2014-05-11 02:18:33 +0200

    The demo is now interactive

--- examples/generative-music-experiment/js/musicgenerator.js
+++ examples/generative-music-experiment/js/musicgenerator.js
@@ -65,4 +65,36 @@ function generateAndPlay() {
 
     var app = new SchedulerApp();
 
-}
+};
+
+function playTest() {
+    var Synth = function(audiolet, frequency) {
+        AudioletGroup.call(this, audiolet, 0, 1);
+        // Basic wave
+        this.saw = new Saw(audiolet, frequency);
+
+        // Gain envelope
+        this.gain = new Gain(audiolet);
+        this.env = new PercussiveEnvelope(audiolet, 1, 0.1, 0.1,
+            function() {
+                this.audiolet.scheduler.addRelative(0,
+                    this.remove.bind(this));
+            }.bind(this)
+        );
+
+        // Main signal path
+        this.saw.connect(this.gain);
+        this.gain.connect(this.outputs[0]);
+
+        // Envelope
+        this.env.connect(this.gain, 0, 1);
+    }
+    extend(Synth, AudioletGroup);
+
+    var audiolet = new Audiolet();
+    var inputEl = document.getElementById("frequencyInput");
+    var frequency  = parseInt(inputEl.value);
+    var synth = new Synth(audiolet, frequency);
+    synth.connect(audiolet.output);
+
+};
